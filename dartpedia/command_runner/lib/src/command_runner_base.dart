ea23f6b10456543e085336a8486945d3f5651cbe
Egor Malyshev <ranyad@mail.ru>
2026-04-06 17:28:57 +0300

    4 урок. Реализуем класс CommandRunner с методом run в пакете command_runner

--- dartpedia/command_runner/lib/src/command_runner_base.dart
+++ dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -1,6 +1,8 @@
-// TODO: Put public facing types in this file.
-
-/// Checks if you are awesome. Spoiler: you are.
 class Awesome {
   bool get isAwesome => true;
+}
+class CommandRunner {
+  Future<void> run(List<String> input) async {
+    print('CommandRunner received arguments: $input');
+  }
 }
