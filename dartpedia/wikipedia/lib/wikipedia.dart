24f2c2de13001013de66ed1b04a6244b3f9d1d9b
Egor Malyshev <ranyad@mail.ru>
2026-04-06 19:50:42 +0300

    Урок 9.Обновим экспорты wikipedia.dart для всех моделей данных

--- dartpedia/wikipedia/lib/wikipedia.dart
+++ dartpedia/wikipedia/lib/wikipedia.dart
@@ -1,3 +1,6 @@
-int calculate() {
-  return 6 * 7;
-}
+library;
+
+export 'src/model/article.dart';
+export 'src/model/search_results.dart';
+export 'src/model/summary.dart';
+export 'src/model/title_set.dart';
