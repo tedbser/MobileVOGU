f4eb6438e681d6ad607d1bbee86919845f6da514
Egor Malyshev <ranyad@mail.ru>
2026-04-06 16:59:32 +0300

    2 урок. Заглушка для команды search

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -2,15 +2,19 @@ const version = '0.0.1';
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); 
+    printUsage();
   } 
   else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  } 
+  }
+  else if (arguments.first == 'search') {
+    print('Команда search распознана!');
+  }
   else {
     printUsage();
   }
 }
+
 void printUsage() {
   print(
     "Доступные команды: 'help', 'version', 'search <Название_Статьи>'"
