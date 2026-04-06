7f4a84feb7e96c984dc893c1b39965a02ac989a5
Egor Malyshev <ranyad@mail.ru>
2026-04-06 17:15:56 +0300

    3 урок. Добавить импорт http

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,22 +1,17 @@
-// bin/cli.dart
-
-import 'dart:io'; 
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
     printUsage();
-  } 
-  else if (arguments.first == 'version') {
+  } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  }
-  else if (arguments.first == 'search') {
+  } else if (arguments.first == 'search') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputArgs);
-  }
-
-  else {
+  } else {
     printUsage();
   }
 }
@@ -24,13 +19,13 @@ void main(List<String> arguments) {
 void searchWikipedia(List<String>? arguments) {
   final String articleTitle;
 
-
   if (arguments == null || arguments.isEmpty) {
     print('Пожалуйста, введите название статьи.');
     articleTitle = stdin.readLineSync() ?? '';
   } else {
     articleTitle = arguments.join(' ');
   }
+
   print('Ищем статьи о "$articleTitle". Пожалуйста, подождите.');
   print('Вот что нашлось:');
   print('(Представьте, что это настоящая статья о "$articleTitle")');
