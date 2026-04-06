d626aafbe9ed535f7287be75711b4590e7e09d5c
Egor Malyshev <ranyad@mail.ru>
2026-04-06 20:14:01 +0300

    Урок 10.Добавить тестовые JSON файлы для десериализации данных Wikipedia API

--- dartpedia/wikipedia/test/wikipedia_test.dart
+++ dartpedia/wikipedia/test/wikipedia_test.dart
@@ -1,8 +1,11 @@
-import 'package:wikipedia/wikipedia.dart';
-import 'package:test/test.dart';
+import 'dart:convert';
+import 'dart:io';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+import 'package:test/test.dart';
+import 'package:wikipedia/src/model/article.dart';
+import 'package:wikipedia/src/model/search_results.dart';
+import 'package:wikipedia/src/model/summary.dart';
+
+const String dartLangSummaryJson = './test/test_data/dart_lang_summary.json';
+const String catExtractJson = './test/test_data/cat_extract.json';
+const String openSearchResponse = './test/test_data/open_search_response.json';
