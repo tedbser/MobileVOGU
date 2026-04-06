0a779821a1df8e30f9a2e8c69e6860c385f0c873
Egor Malyshev <ranyad@mail.ru>
2026-04-06 16:30:29 +0300

    printUsage и команда help

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,10 +1,18 @@
 const version = '0.0.1';
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print('Hello, Dart!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage(); 
   } 
   else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } 
+  else {
+    printUsage();
   }
+}
+void printUsage() {
+  print(
+    "Доступные команды: 'help', 'version', 'search <Название_Статьи>'"
+  );
 }
