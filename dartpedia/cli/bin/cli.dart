648e79d978ea8fd1987e079ec7a2d4dc11379101
Egor Malyshev <ranyad@mail.ru>
2026-04-06 17:55:22 +0300

    Урок 5. Обновим cli.dart: добавить HelpCommand в CommandRunner и удалить старые импорты

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,8 +1,8 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart';
 
-void main(List<String> arguments) async {
-  var runner = CommandRunner();
-  await runner.run(arguments);
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
