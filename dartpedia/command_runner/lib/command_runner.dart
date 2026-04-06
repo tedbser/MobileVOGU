3d98af3e974d7aea8dc943966d3bba2041f99a12
Egor Malyshev <ranyad@mail.ru>
2026-04-06 19:04:31 +0300

    Урок 7.Добавить экспорт в комманд раннер

--- dartpedia/command_runner/lib/command_runner.dart
+++ dartpedia/command_runner/lib/command_runner.dart
@@ -2,5 +2,6 @@ library;
 
 export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
-export 'src/help_command.dart';
 export 'src/exceptions.dart';
+export 'src/help_command.dart';
+export 'src/console.dart';
