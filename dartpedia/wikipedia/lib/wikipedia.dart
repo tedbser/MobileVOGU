e47c7c9b114c1778e6f6990d1a0ff4397aacf750
Egor Malyshev <ranyad@mail.ru>
2026-04-06 20:49:13 +0300

    Урок 11.Обновить экспорты, добавить АПИ функции в публичное АПИ пакета Википедия

--- dartpedia/wikipedia/lib/wikipedia.dart
+++ dartpedia/wikipedia/lib/wikipedia.dart
@@ -2,3 +2,10 @@ library;
 
 export 'src/model/article.dart';
 export 'src/model/search_results.dart';
+export 'src/api/get_article.dart';
+export 'src/api/search.dart';
+export 'src/api/summary.dart';
+export 'src/model/article.dart';
+export 'src/model/search_results.dart';
+export 'src/model/summary.dart';
+export 'src/model/title_set.dart';
